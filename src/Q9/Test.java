8dfe9a912d98279cb87a096b4df942b137433710
osoka46 <osman.bozdag@yahoo.com>
2022-05-17 23:13:30 +0200

    ilk 10 soru hazir

--- src/Q9/Test.java
+++ src/Q9/Test.java
@@ -1,4 +1,32 @@
 package Q9;
 
+class CD{
+    int r;
+    CD (int r)
+    {
+        this.r=r;
+    }
+}
+/*class DVD extends CD
+{
+    int c;
+    DVD(int r, int c)
+    {
+        //line n1
+
+    }
+}*/
+
 public class Test {
+    public static void main(String[] args) {
+        //DVD dvd=new DVD(10,20);
+
+        /*which code fragment should you use line n1 to instantiate dvd objet?
+        a. super.r=r; this.c=c;
+        b. super(r); this(c);
+        c. super(r); this.c=c;
+        d. this.c=r; super (c);
+        * */
+//cevap, c
+    }
 }
