409de1118c613ebd37e5d7e4c4963ab2b34d7537
Yasin Ekin <ekinyasin90@gmail.com>
2022-05-18 23:09:40 +0300

    new changes

---
+++ src/Q80/Test.java
@@ -0,0 +1,12 @@
+package Q80;
+
+public class Test {
+    /*80. Which two features can be implemented in a Java application by encapsulating the entity classes used?
+(Choose two.)
+A. data validation
+B. compile time polymorphism
+C. data hiding
+D. data abstraction
+E. data memory optimization
+Answer: C D*/
+}
