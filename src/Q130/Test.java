268111b2ffca9bad05d242f2063a35404ed3c3b0
osoka46 <osman.bozdag@yahoo.com>
2022-05-19 13:54:54 +0200

    125-136

---
+++ src/Q130/Test.java
@@ -0,0 +1,11 @@
+package Q130;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("5+2="+3+4);
+        System.out.println("5+2="+(3+4));
+
+        //what is result
+        //cevap, 5+2=34;   5+2=7;
+    }
+}
