47e6b4e77549c062f3e883810f3615c1c912566b
Yasin Ekin <ekinyasin90@gmail.com>
2022-05-19 12:39:24 +0300

    new changes

---
+++ src/Q95/Shop.java
@@ -0,0 +1,29 @@
+package Q95;
+
+
+/*import java.util.ArrayList;
+import java.util.List;
+
+class Product {
+    int id;
+ 	String name;
+ 	Product (int id, String name) {
+ 		this.id = id;
+		this.name = name;
+ 	}
+}
+public class Shop {
+ 	public static void main(String[] args) {
+ 		List<Product> lst =new ArrayList<>();
+ 		lst.add(new Product(10, "IceCream)"));
+ 		lst.add(new Product(11,"Chocolate"));
+ 		Product p1 = new Product(10, "IceCream)");
+		System.out.println(lst.indexOf(p1));
+ 	}
+ }*/
+/*What is the result?
+A. true
+B. false
+C. -1
+D. 0
+Answer: C*/
