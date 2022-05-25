f03370f29173a2de3673368c98d6b3382d6f5cd1
BİLAL <bllsfk.3984@gmail.com>
2022-05-25 15:58:41 +0300

    committed by Bilal

---
+++ src/Q174/MyString.java
@@ -0,0 +1,40 @@
+package Q174;
+
+class MyString {
+    String msg;
+
+    MyString(String msg) {
+        this.msg = msg;
+    }
+}
+//Test.java:
+class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello " + new StringBuilder("Java SE 8"));
+        System.out.println("Hello " + new MyString("Java SE 8").msg);
+
+    }
+}
+/*
+    What is
+    the result?
+
+        a)Hello Java SE 8
+        Hello Java SE 8
+
+        b)Hello java.lang.StringBuilder@<<hashcode1>>
+        Hello p1.MyString@<<hashcode2>>
+
+        c)Hello Java SE 8
+        Hello p1.MyString@<<hashcode>>
+
+        d)Compilation fails at the Test
+
+
+        A.Option A
+        B.Option B
+        C.Option C
+        D.Option D
+        E.Option E
+
+        Answer:A*/
